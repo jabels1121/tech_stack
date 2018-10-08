14402d5124b071f537981e512e6c094f2ad92e7e
Jabels <jabels1121@gmail.com>
2018-10-08 17:09:08 +0300

    Created Header of application using my previosly created common header component

--- src/App.js
+++ src/App.js
@@ -3,19 +3,13 @@ import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
+import { Header } from './components/common/exportComponent';
 
 const App = () => {
     return(
         <Provider store={createStore(reducers)}>
-            <View style={
-                {
-                    flex: 1,
-                    flexDirection: 'row',
-                    justifyContent: 'center',
-                    alignItems: 'center'
-                }
-            }>
-                <Text>Hello World!</Text>
+            <View>
+                <Header headerText='Tech Stack' />
             </View>
         </Provider>
     );
