a66c92227121ada114a48b54dadd87e6c8c58c11
Jabels <jabels1121@gmail.com>
2018-10-08 17:35:55 +0300

    Created library list in json file which contain static data for representing in out app. And created Library reducer that every time returns list of data.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import {View} from 'react-native';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
 import reducers from './reducers';
-import { Header } from './components/common/exportComponent';
+import {Header} from './components/common/exportComponent';
 
 const App = () => {
     return(
